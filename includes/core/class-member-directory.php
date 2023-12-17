2179c8fb77a3d35a10f57b15e1046a6bbb6968a0
umdevelopera <umdevelopera@gmail.com>
2023-12-17 16:56:16 +0200

    - prepare version 1.1.0

--- includes/core/class-member-directory.php
+++ includes/core/class-member-directory.php
@@ -27,7 +27,11 @@ if ( ! class_exists( 'um_ext\um_optimize\core\Member_Directory' ) ) {
 		 * Class constructor
 		 */
 		public function __construct() {
+			// Member directory with the default usermeta table.
 			add_filter( 'get_meta_sql', array( $this, 'get_meta_sql' ), 10, 6 );
+
+			// Member directory with the custom table for usermeta.
+			add_filter( 'um_query_args_filter_global_meta', array( $this, 'handle_filter_meta_sql' ), 10, 6 );
 		}
 
 
@@ -55,9 +59,8 @@ if ( ! class_exists( 'um_ext\um_optimize\core\Member_Directory' ) ) {
 		 *     @type string $where SQL fragment to append to the main WHERE clause.
 		 * }
 		 */
-		function get_meta_sql( $sql, $queries, $type, $primary_table, $primary_id_column, $context ) {
+		public function get_meta_sql ( $sql, $queries, $type, $primary_table, $primary_id_column, $context ) {
 			if ( defined( 'DOING_AJAX' ) && DOING_AJAX
-				&& defined( 'WP_DEBUG' ) && WP_DEBUG
 				&& UM()->options()->get( 'members_page' )
 				&& UM()->options()->get( 'um_optimize_members' )
 				&& 'user' === $type
@@ -74,6 +77,181 @@ if ( ! class_exists( 'um_ext\um_optimize\core\Member_Directory' ) ) {
 				return $sql_um;
 			}
 			return $sql;
+		}
+
+
+		/**
+		 * Changes SQL clauses for filtering members in the Member Directory.
+		 *
+		 * @since 1.1.1
+		 *
+		 * @see \um\core\Member_Directory_Meta::handle_filter_query()
+		 *
+		 * @param bool   $skip_default Skip default JOIN for this field if TRUE.
+		 * @param object $um_mdm       The \um\core\Member_Directory_Meta class.
+		 * @param string $field        Meta Key of the field.
+		 * @param mixed  $value        Value(s) of the field.
+		 * @param string $filter_type  Filter type.
+		 * @param bool   $is_default
+		 *
+		 * @return bool
+		 */
+		public function handle_filter_meta_sql ( $skip_default, $um_mdm, $field, $value, $filter_type, $is_default ) {
+			global $wpdb;
+
+			if (  defined( 'DOING_AJAX' ) && DOING_AJAX
+				&& UM()->options()->get( 'um_optimize_members' )
+				&& false === $skip_default ) {
+
+				$alias        = $wpdb->prepare( '%i', 'umm_' . $field );
+				$skip_default = true;
+
+				switch ( $filter_type ) {
+
+					default:
+						$skip_default = false;
+						break;
+
+					case 'text':
+						$compare = apply_filters( 'um_members_directory_filter_text', '=', $field );
+						$value   = apply_filters( 'um_members_directory_filter_text_meta_value', trim( stripslashes( $value ) ), $field );
+
+						$um_mdm->joins[] = $wpdb->prepare(
+							"INNER JOIN {$wpdb->prefix}um_metadata AS {$alias} ON ( {$alias}.user_id = u.ID AND {$alias}.um_key = %s )",
+							$field
+						);
+
+						$um_mdm->where_clauses[] = $wpdb->prepare(
+							"{$alias}.um_key = %s AND {$alias}.um_value {$compare} %s",
+							$field,
+							$value
+						);
+
+						if ( ! $is_default ) {
+							$um_mdm->custom_filters_in_query[ $field ] = $value;
+						}
+						break;
+
+					case 'select':
+						if ( ! is_array( $value ) ) {
+							$value = array( $value );
+						}
+
+						$values_array = array();
+						foreach ( $value as $single_val ) {
+							$single_val = trim( stripslashes( $single_val ) );
+
+							$values_array[] = $wpdb->prepare( "{$alias}.um_value = %s", $single_val );
+							$values_array[] = $wpdb->prepare( "{$alias}.um_value LIKE %s", '%"' . $single_val . '"%' );
+							$values_array[] = $wpdb->prepare( "{$alias}.um_value LIKE %s", '%' . serialize( (string) $single_val ) . '%' );
+
+							if ( is_numeric( $single_val ) ) {
+								$values_array[] = $wpdb->prepare( "{$alias}.um_value LIKE %s", '%' . serialize( (int) $single_val ) . '%' );
+							}
+						}
+						$values = implode( ' OR ', $values_array );
+
+						$um_mdm->joins[] = $wpdb->prepare(
+							"INNER JOIN {$wpdb->prefix}um_metadata AS {$alias} ON ( {$alias}.user_id = u.ID AND {$alias}.um_key = %s )",
+							$field
+						);
+
+						$um_mdm->where_clauses[] = $wpdb->prepare(
+							"( {$alias}.um_key = %s AND ( {$values} ) )",
+							$field
+						);
+
+						if ( ! $is_default ) {
+							$um_mdm->custom_filters_in_query[ $field ] = $value;
+						}
+						break;
+
+					case 'slider':
+						$min = min( $value );
+						$max = max( $value );
+
+						$um_mdm->joins[] = $wpdb->prepare(
+							"INNER JOIN {$wpdb->prefix}um_metadata AS {$alias} ON ( {$alias}.user_id = u.ID AND {$alias}.um_key = %s )",
+							$field
+						);
+
+						$um_mdm->where_clauses[] = $wpdb->prepare(
+							"( {$alias}.um_key = %s AND {$alias}.um_value BETWEEN %d AND %d )",
+							$field,
+							$min,
+							$max
+						);
+
+						if ( ! $is_default ) {
+							$um_mdm->custom_filters_in_query[ $field ] = $value;
+						}
+						break;
+
+					case 'datepicker':
+						$offset = 0;
+						if ( ! $is_default ) {
+							if ( isset( $_POST['gmt_offset'] ) && is_numeric( $_POST['gmt_offset'] ) ) {
+								$offset = (int) $_POST['gmt_offset'];
+							}
+						} else {
+							$gmt_offset = get_post_meta( $directory_data['form_id'], '_um_search_filters_gmt', true );
+							if ( is_numeric( $gmt_offset ) ) {
+								$offset = (int) $gmt_offset;
+							}
+						}
+
+						$from_time = (int) min( $value ) + ( $offset * HOUR_IN_SECONDS ); // client time zone offset
+						$to_time   = (int) max( $value ) + ( $offset * HOUR_IN_SECONDS ) + DAY_IN_SECONDS - 1; // time 23:59
+						$from_date = date( 'Y/m/d', $from_time );
+						$to_date   = date( 'Y/m/d', $to_time );
+
+						$um_mdm->joins[] = $wpdb->prepare(
+							"INNER JOIN {$wpdb->prefix}um_metadata AS {$alias} ON ( {$alias}.user_id = u.ID AND {$alias}.um_key = %s )",
+							$field
+						);
+
+						$um_mdm->where_clauses[] = $wpdb->prepare(
+							"( {$alias}.um_key = %s AND {$alias}.um_value BETWEEN %s AND %s )",
+							$field,
+							$from_date,
+							$to_date
+						);
+
+						if ( ! $is_default ) {
+							$um_mdm->custom_filters_in_query[ $field ] = array( $from_date, $to_date );
+						}
+						break;
+
+					case 'timepicker':
+
+						$um_mdm->joins[] = $wpdb->prepare(
+							"INNER JOIN {$wpdb->prefix}um_metadata AS {$alias} ON ( {$alias}.user_id = u.ID AND {$alias}.um_key = %s )",
+							$field
+						);
+
+						if ( $value[0] === $value[1] ) {
+							$um_mdm->where_clauses[] = $wpdb->prepare(
+								"( {$alias}.um_key = %s AND {$alias}.um_value = %s )",
+								$field,
+								$value[0]
+							);
+						} else {
+							$um_mdm->where_clauses[] = $wpdb->prepare(
+								"( {$alias}.um_key = %s AND CAST( {$alias}.um_value AS TIME ) BETWEEN %s AND %s )",
+								$field,
+								$value[0],
+								$value[1]
+							);
+						}
+
+						if ( ! $is_default ) {
+							$um_mdm->custom_filters_in_query[ $field ] = $value;
+						}
+						break;
+				}
+			}
+
+			return $skip_default;
 		}
 
 	}
--- readme.txt
+++ readme.txt
@@ -5,10 +5,10 @@ Author URI: https://github.com/umdevelopera
 Plugin URI: https://github.com/umdevelopera/um-optimize
 Tags: ultimate member, optimize, assets
 Requires at least: 5.5
-Tested up to: 6.4.1
+Tested up to: 6.4.2
 Requires UM core at least: 2.6.8
 Tested UM core up to: 2.8.0
-Stable tag: 1.1.0
+Stable tag: 1.1.1
 License: GNU Version 2 or Any Later Version
 License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 
@@ -20,12 +20,13 @@ Optimize loading for sites with the Ultimate Member plugin.
 
 * Removes CSS and JS files that belongs to Ultimate Member and its extensions from pages where there are no Ultimate Member elements.
 * Combines CSS and JS files that belongs to Ultimate Member and its extensions on pages with Ultimate Member elements.
-* Minifies combined files.
 * Optimizes SQL queries to get posts and users faster.
 
 = Documentation & Support =
 
-This is a free extension created for the community. The Ultimate Member team does not provide support for this extension. Open new issue in the GitHub repository if you face a problem: https://github.com/umdevelopera/um-optimize/issues
+This is a free extension created for the community. The Ultimate Member team does not provide support for this extension.
+Open new issue in the GitHub repository if you are facing a problem or have a suggestion: https://github.com/umdevelopera/um-optimize/issues
+Documentation is the README section in the GitHub repository: https://github.com/umdevelopera/um-optimize
 
 == Installation ==
 
@@ -34,6 +35,13 @@ Download ZIP file from GitHub or Google Drive. You can find download links here:
 You can install this plugin from the ZIP file as any other plugin. Follow this instruction: https://wordpress.org/support/article/managing-plugins/#upload-via-wordpress-admin
 
 == Changelog ==
+
+= 1.1.1: December 17, 2023 =
+
+* Fixed: Add non-UM dependencies to combined files.
+* Fixed: Load combined footer scripts in footer.
+* Fixed: Sanitize table alias in SQL requests.
+* Tweak: The "Speed up member directories" is available in any mode.
 
 = 1.1.0: December 11, 2023 =
 
